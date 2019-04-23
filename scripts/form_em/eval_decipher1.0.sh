4ae0432259accbee2977ec7d98c345449873652d
jxhe <junxianh2@gmail.com>
2019-04-23 15:18:49 -0400

    clean

--- scripts/form_em/eval_decipher1.0.sh
+++
@@ -1,7 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-
-printf "\ntranslate cipher test\n"
-./scripts/translate.sh $1 data/yelp_decipher/yelp_decipher1.0/decipher.test.1 data/yelp_decipher/yelp_decipher1.0/test_1.attr transfer_plain.txt
-
-path=$1transfer_plain.txt
-./multi-bleu.perl data/yelp_decipher/yelp_decipher1.0/decipher.test.0 < $path
